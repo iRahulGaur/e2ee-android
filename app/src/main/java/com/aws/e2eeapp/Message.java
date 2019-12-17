88a274e57215e20b9f21ffba7459aaeca7ddc4e0
Rahul Gaur <rahul.gaur152@gmail.com>
2019-12-17 11:22:01 +0530

    now, 2 methods are added steps this app do 1. create AES secretKey using secretMessage 2. encrypt the data using AES 3. encrypt the secretMessage with RSA to create public and private key 4. use public key to decrypt secretMessage and create AESKey 5. decrypt the message using AESKey

--- app/src/main/java/com/aws/e2eeapp/Message.java
+++ app/src/main/java/com/aws/e2eeapp/Message.java
@@ -21,7 +21,6 @@ import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -50,6 +49,8 @@ public class Message extends AppCompatActivity {
     private PrivateKey privateKey;
     byte[] encryptedBytes = null;
     private static final String TAG = "Message";
+    private String secretKey = "this is a secret key";
+    private String encryptedMessage;
 
     public PublicKey publicKey;
 
@@ -68,30 +69,29 @@ public class Message extends AppCompatActivity {
             privateKey = keyPair.getPrivate();
             publicKey = keyPair.getPublic();
 
-            try {
-                // Creates a file in the primary external storage space of the
-                // current application.
-                // If the file does not exists, it is created.
-                File testFile = new File(this.getExternalFilesDir(null), "key.pub");
+            // Creates a file in the primary external storage space of the
+            // current application.
+            // If the file does not exists, it is created.
+            File testFile = new File(this.getExternalFilesDir(null), "key.pub");
 
-                // Adds a line to the file
-                FileOutputStream writer = new FileOutputStream(testFile);
-                writer.write(publicKey.getEncoded());
-                writer.close();
-                // Refresh the data so it can seen when the device is plugged in a
-                // computer. You may have to unplug and replug the device to see the
-                // latest changes. This is not necessary if the user should not modify
-                // the files.
-                MediaScannerConnection.scanFile(this,
-                        new String[]{testFile.toString()},
-                        null,
-                        null);
-            } catch (IOException e) {
-                Log.e("ReadWriteFile", "Unable to write to the TestFile.txt file.");
-            }
+            // Adds a line to the file
+            FileOutputStream writer = new FileOutputStream(testFile);
+            writer.write(publicKey.getEncoded());
+            writer.close();
+            // Refresh the data so it can seen when the device is plugged in a
+            // computer. You may have to unplug and replug the device to see the
+            // latest changes. This is not necessary if the user should not modify
+            // the files.
+            MediaScannerConnection.scanFile(this,
+                    new String[]{testFile.toString()},
+                    null,
+                    null);
+
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             Log.e(TAG, "initKeys: exception " + e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -110,10 +110,14 @@ public class Message extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     private void getMessage() {
         try {
-            String decryptedString = Utils.decrypt(encryptedBytes, privateKey);
+            String decryptedSecretKey = Utils.decrypt(encryptedBytes, privateKey);
 
-            Log.e(TAG, "getMessage: data decrypted " + decryptedString);
-            decryptedTextView.setText(decryptedString + "");
+            Log.e(TAG, "getMessage: this is secret key used to encrypt the data : " + decryptedSecretKey);
+
+            Log.e(TAG, "sendMessage: AES encrypted message in get Message : " + encryptedMessage);
+
+            String decryptedMessage = Utils.AESDecryptionString(encryptedMessage, decryptedSecretKey);
+            decryptedTextView.setText(decryptedMessage + "");
         } catch (Exception e) {
             e.printStackTrace();
             Log.e(TAG, "getMessage: exception in decryption ");
@@ -128,11 +132,15 @@ public class Message extends AppCompatActivity {
         if (!message.isEmpty()) {
             try {
 
+                encryptedMessage = Utils.AESEncryptionString(message, secretKey);
+                Log.e(TAG, "sendMessage: AES encrypted message " + encryptedMessage);
+                String dec = Utils.AESDecryptionString(encryptedMessage, secretKey);
+                Log.e(TAG, "sendMessage: this is string decrypted in sendMessage " + dec);
+
                 // Gets the file from the primary external storage space of the
                 // current application.
                 File testFile = new File(this.getExternalFilesDir(null), "key.pub");
                 // Reads the data from the file
-                Log.e(TAG, "sendMessage: this is pub encoded " + Arrays.toString(publicKey.getEncoded()));
 
                 FileInputStream reader;
                 reader = new FileInputStream(testFile);
@@ -144,13 +152,13 @@ public class Message extends AppCompatActivity {
                     os.write(b, 0, c);
                 }
 
-                Log.e(TAG, "sendMessage: os bytes " + Arrays.toString(os.toByteArray()));
+               //Log.e(TAG, "sendMessage: os bytes " + Arrays.toString(os.toByteArray()));
 
                 byte[] pubKeyEncoded = os.toByteArray();
 
                 reader.close();
 
-                encryptedBytes = Utils.encrypt(message, pubKeyEncoded);
+                encryptedBytes = Utils.encrypt(secretKey, pubKeyEncoded);
                 String encryptedString = new String(encryptedBytes);
                 Toast.makeText(this, "Message Encrypted ", Toast.LENGTH_SHORT).show();
                 encryptedTextView.setText(encryptedString);
--- app/src/main/java/com/aws/e2eeapp/Utils.java
+++ app/src/main/java/com/aws/e2eeapp/Utils.java
@@ -1,17 +1,25 @@
 package com.aws.e2eeapp;
 
+import android.annotation.SuppressLint;
 import android.util.Log;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 import static android.content.ContentValues.TAG;
 
@@ -23,6 +31,10 @@ import static android.content.ContentValues.TAG;
 class Utils {
 
     private static final String ALGORITHM = "RSA";
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private static final String AES_PADDING = "AES/ECB/PKCS5PADDING";
+    private static final String RSA__PADDING = "RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING";
 
     /*
     * Padding sizes
@@ -41,7 +53,9 @@ class Utils {
         RSA/ECB/OAEPWithSHA3-512AndMGF1Padding, 130
     *
     * real data size will be = (block size / 8) - paddingSize
-    * in our case = (2048 / 8) - 42 = 214 (char)
+    * in our case = (2048 / 8) - 66  = 190 (char)
+    *
+    * AES padding = AES/ECB/PKCS5PADDING
     * */
 
     static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
@@ -52,14 +66,14 @@ class Utils {
 
         KeyPair generateKeyPair = keyGen.generateKeyPair();
 
-        Log.e(TAG, "generateKeyPair: this is public key encoding " + Arrays.toString(generateKeyPair.getPrivate().getEncoded()));
+        //Log.e(TAG, "generateKeyPair: this is public key encoding " + Arrays.toString(generateKeyPair.getPrivate().getEncoded()));
 
         return generateKeyPair;
     }
 
     static byte[] encrypt(String plainText, byte[] publicKeyEncoded) throws Exception {
-        //Get Cipher Instance RSA With ECB Mode and OAEPWITHSHA-1ANDMGF1PADDING Padding
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-1ANDMGF1PADDING");
+        //Get Cipher Instance RSA With ECB Mode and OAEPWithSHA3-256AndMGF1Padding Padding
+        Cipher cipher = Cipher.getInstance(RSA__PADDING);
 
         PublicKey publicKey =
                 KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKeyEncoded));
@@ -73,8 +87,8 @@ class Utils {
     }
 
     static String decrypt(byte[] cipherTextArray, PrivateKey privateKey) throws Exception {
-        //Get Cipher Instance RSA With ECB Mode and OAEPWITHSHA-1ANDMGF1PADDING Padding
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-1ANDMGF1PADDING");
+        //Get Cipher Instance RSA With ECB Mode and OAEPWithSHA3-256AndMGF1Padding Padding
+        Cipher cipher = Cipher.getInstance(RSA__PADDING);
 
         //Initialize Cipher for DECRYPT_MODE
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
@@ -84,4 +98,97 @@ class Utils {
 
         return new String(decryptedTextArray);
     }
+
+    private static void setKey(String myKey) {
+        MessageDigest sha;
+        try {
+            key = myKey.getBytes(StandardCharsets.UTF_8);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+            Log.e(TAG, "setKey: this is AES secretKey = " + Arrays.toString(secretKey.getEncoded()));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+/*
+    static String encrypt(String strToEncrypt, String secret) {
+        try {
+            setKey(secret);
+            @SuppressLint("GetInstance") Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(strToEncrypt.getBytes()));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    static String decrypt(String strToDecrypt, String secret) {
+        try {
+            setKey(secret);
+            @SuppressLint("GetInstance") Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            byte[] text = cipher.doFinal(strToDecrypt.getBytes());
+            return new String(text);
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+    */
+
+    //for encryption
+    @SuppressLint("GetInstance")
+    static String AESEncryptionString(String stringData, String secret) {
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance(AES_PADDING);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        setKey(secret);
+        byte[] stringToByte = stringData.getBytes();
+        byte[] encryptionByte = new byte[stringToByte.length];
+
+        try {
+            assert cipher != null;
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            encryptionByte = cipher.doFinal(stringToByte);
+
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+        String returnData;
+        returnData = new String(encryptionByte, StandardCharsets.ISO_8859_1);
+        return returnData;
+    }
+
+    //DecryptString
+    @SuppressLint("GetInstance")
+    static String AESDecryptionString(String stringData, String secret) {
+        Cipher decipher = null;
+        byte[] encryptedString = stringData.getBytes(StandardCharsets.ISO_8859_1);
+        String returnData = stringData;
+        setKey(secret);
+        try {
+            decipher = Cipher.getInstance(AES_PADDING);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+        byte[] decryption;
+        try {
+            assert decipher != null;
+            decipher.init(Cipher.DECRYPT_MODE, secretKey);
+            decryption = decipher.doFinal(encryptedString);
+            returnData = new String(decryption);
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return returnData;
+    }
+
 }
